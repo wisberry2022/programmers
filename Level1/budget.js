fbdfbb4ef02fd53dc2d8a1ff9cc5cb94b143ceb4
inseo <wisberry2022@gmail.com>
2022-11-30 17:35:02 +0900

    11/30 upload

--- Level1/budget.js
+++ Level1/budget.js
@@ -1,9 +1,17 @@
 const solution = (d, budget) => {
-  return d.sort((a, b) => a - b).reduce((count, price) => {
-    return count + ((budget -= price) >= 0);
-  }, 0)
+  //22-11-29
+  const dsort = d.sort((a, b) => a - b);
+  // return dsort.reduce((acc, cur) => acc + cur) <= budget ? dsort.length : solution(dsort.slice(0, dsort.length - 1), budget);
+  let support = 0;
+  for (let i of dsort) {
+    budget -= i;
+    if (budget < 0) {
+      return support;
+    }
+    support++;
+  }
 }
 
-console.log(solution([1, 5, 4, 3, 2], 9))
+console.log(solution([1, 5, 4, 3, 2], 5))
 console.log(solution([1, 3, 2, 5, 4], 9))
 console.log(solution([2, 1, 7, 35, 34, 36, 12, 3500, 300, 20500], 500))

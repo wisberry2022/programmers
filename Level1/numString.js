7c58725dea3f60523fb618d6353d77de362028aa
wisberry2022 <wisberry2022@gmail.com>
2022-12-15 21:49:37 +0900

    12/15 upload

---
+++ Level1/numString.js
@@ -0,0 +1,11 @@
+const solution = (s) => {
+  // 22-12-15
+  const charSet = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  charSet.forEach((it, idx) => {
+    const reg = new RegExp(`${it}`, 'g');
+    s = s.replace(reg, String(idx));
+  })
+  console.log(Number(s));
+}
+
+solution("1zerotwozero3");
